ffc352192fbcdd45ee5035fe3d49a3dc95cb3656
wilson-ye-chen <ye.chen@sydney.edu.au>
2023-05-08 09:50:22 +0100

    FIX: bug in KGM base kernel

--- jaxtein/kernel.py
+++ jaxtein/kernel.py
@@ -12,5 +12,5 @@ def k_kgm(x, y, s, l, cinv):
     y = y - l
     k = (1 + n2(x)) ** t * (1 + n2(y)) ** t * (1 + n2(x - y)) ** (-0.5) + \
         (1 + jnp.dot(jnp.dot(x, cinv), y)) / \
-        (1 + n2(x)) ** 0.5 * (1 + n2(y)) ** 0.5
+        ((1 + n2(x)) ** 0.5 * (1 + n2(y)) ** 0.5)
     return k
