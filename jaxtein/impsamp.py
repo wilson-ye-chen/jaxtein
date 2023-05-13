c216f20c58debb1074e6ffc4535750d08e5ddd0d
wilson-ye-chen <ye.chen@sydney.edu.au>
2023-05-13 15:59:24 +0100

    MOVE: PiDistribution to SIS

--- jaxtein/impsamp.py
+++ jaxtein/impsamp.py
@@ -42,3 +42,18 @@ class SteinImportanceSampling:
         # Solve
         w = solve_qp(k0f, q, g, h, a, b, solver='proxqp')
         return (w, x, k0f)
+
+class PiDistribution:
+    def __init__(self, logp, dlogp, d2logp, k0):
+        self.logp = logp
+        self.dlogp = dlogp
+        self.d2logp = d2logp
+        self.k0 = k0
+        self.k0xx = lambda x: k0.evaluate(x, x)
+        self.dk0xx = jit(jacfwd(self.k0xx))
+
+    def logpdf(self, x):
+        return self.logp(x) + 0.5 * jnp.log(self.k0xx(x))
+
+    def dlogpdf(self, x):
+        return self.dlogp(x) + 0.5 * self.dk0xx(x) / self.k0xx(x)
--- jaxtein/thinning.py
+++ jaxtein/thinning.py
@@ -23,18 +23,3 @@ class SteinThinning:
             k0m[:, i] = self.vfk0(x, x_last)
             idx[i] = np.argmin(k0m[:, 0] + 2 * np.sum(k0m[:, 1:(i + 1)], axis=1))
         return idx
-
-class PiDistribution:
-    def __init__(self, logp, dlogp, d2logp, k0):
-        self.logp = logp
-        self.dlogp = dlogp
-        self.d2logp = d2logp
-        self.k0 = k0
-        self.k0xx = lambda x: k0.evaluate(x, x)
-        self.dk0xx = jit(jacfwd(self.k0xx))
-
-    def logpdf(self, x):
-        return self.logp(x) + 0.5 * jnp.log(self.k0xx(x))
-
-    def dlogpdf(self, x):
-        return self.dlogp(x) + 0.5 * self.dk0xx(x) / self.k0xx(x)
